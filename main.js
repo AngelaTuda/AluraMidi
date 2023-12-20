8aa9081382f90a9d4ad7d6afd04c83861d397a60
AngelaTuda <100006773+AngelaTuda@users.noreply.github.com>
2023-12-20 18:43:23 -0300

    Projeto finalizado

--- main.js
+++ main.js
@@ -1,33 +1,44 @@
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
-};
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
 
-//document.querySelector('.tecla_pom').onclick=tocaSomPom;
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
+}
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-while(contador + listaDeTeclas.length){
-    const instrumento = listaDeTeclas[contador].classList[1];
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
-    //console.log(instrumento);
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
 
+    tecla.onkeydown = function (evento) {
 
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
 
-    listaDeTeclas[contador].onclick = function(){
-        tocaSom('#som_tecla_pom');
-    };
+    }
 
-    contador = contador + 1;
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 
-    //console.log(contador);
 }
-
-
-
-
-
+    //document.querySelector('.tecla_pom').onclick=tocaSomPom;
+    //console.log(contador);
 
 /*Em let a variável pode ter alteração de valores. Em const significa que o valor é uma constante, 
 ou seja, a variável tem valores imutáveis (que não podem ser alterados).
